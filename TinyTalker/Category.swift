c8ecb8ce81f2c01b953a3147d9d08646dfa97ec9
aawashington <AAWashington@student.fullsail.edu>
2025-08-18 21:22:27 -0400

    added food category

--- TinyTalker/Category.swift
+++ TinyTalker/Category.swift
@@ -24,16 +24,33 @@ struct Category: Identifiable {
 
 // Sample categories used to populate the app
 let sampleCategories: [Category] = [
-    Category(name: "Food", emoji: "🍔", tiles: [
-        //tiles from Needs category as a placeholder
-        Tile(emoji: "🚽", text: "I need to potty"),
-        Tile(emoji: "🍽️", text: "I’m hungry"),
-        Tile(emoji: "🧃", text: "I’m thirsty"),
-        Tile(emoji: "😴", text: "I’m tired"),
-        Tile(emoji: "🙋‍♀️", text: "I need help"),
-        Tile(emoji: "🥵", text: "I’m hot"),
-        Tile(emoji: "🥶", text: "I’m cold"),
-        Tile(emoji: "🙆‍♀️", text: "All done")
+    Category(name: "Food", emoji: "🍳", tiles: [
+        //food category
+        Tile(emoji: "🍎", text: "Apples"),
+        Tile(emoji: "🍌", text: "Bananas"),
+        Tile(emoji: "🍓", text: "Strawberries"),
+        Tile(emoji: "🫐", text: "Blueberries"),
+        Tile(emoji: "🍇", text: "Grapes"),
+        Tile(emoji: "🍉", text: "Watermelon"),
+        Tile(emoji: "🍳", text: "Eggs"),
+        Tile(emoji: "🥞", text: "Pancakes"),
+        Tile(emoji: "🥓", text: "Bacon"),
+        Tile(emoji: "🥒", text: "Cucumbers"),
+        Tile(emoji: "🥦", text: "Broccoli"),
+        Tile(emoji: "🥕", text: "Carrots"),
+        Tile(emoji: "🧀", text: "Cheese"),
+        Tile(emoji: "🥛", text: "Milk"),
+        Tile(emoji: "🧃", text: "Juice"),
+        Tile(emoji: "🌭", text: "Hot Dog"),
+        Tile(emoji: "🍕", text: "Pizza"),
+        Tile(emoji: "🥪", text: "Sandwich"),
+        Tile(emoji: "🍟", text: "Fries"),
+        Tile(emoji: "🍗", text: "Chicken"),
+        Tile(emoji: "🍝", text: "Spaghetti"),
+        Tile(emoji: "🥗", text: "Salad"),
+        Tile(emoji: "🍜", text: "Noodles"),
+        Tile(emoji: "🍪", text: "Cookie"),
+        Tile(emoji: "🍦", text: "Ice Cream"),
     ]),
     Category(name: "Feelings", emoji: "😊", tiles: [
         //tiles from Needs category as a placeholder
